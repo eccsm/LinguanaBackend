87c407462593b43b9c0789a342d9cbe015fd4ff5
eccsm <eccsm34@gmail.com>
2025-12-06 02:40:50 +0300

    feat: add API endpoint for AdMob app-ads.txt verification

--- api/app-ads.js
+++ api/app-ads.js
@@ -1,5 +1,6 @@
 // API endpoint to serve app-ads.txt for AdMob verification
-module.exports = (req, res) => {
+export default function handler(req, res) {
     res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Cache-Control', 's-maxage=86400');
     res.status(200).send('google.com, pub-5060300106472726, DIRECT, f08c47fec0942fa0');
-};
+}
