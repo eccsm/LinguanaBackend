a535ca81648f54325655a7f3d0618e2086a13c64
eccsm <eccsm34@gmail.com>
2025-12-06 02:16:14 +0300

    feat: Add API endpoint for `app-ads.txt` and update Vercel routing to serve it.

---
+++ api/app-ads.txt.js
@@ -0,0 +1,5 @@
+// API endpoint to serve app-ads.txt for AdMob verification
+module.exports = (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send('google.com, pub-5060300106472726, DIRECT, f08c47fec0942fa0');
+};
